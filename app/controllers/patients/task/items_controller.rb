8e8a9cefb7204b992b10df4def78cfeb15a8abb3
Troy Underwood <troyizzle@gmail.com>
2021-08-03 19:24:09 -0400

    Initial commit

--- app/controllers/patients/task/items_controller.rb
+++ app/controllers/patients/task/items_controller.rb
@@ -1,5 +1,5 @@
 class Patients::Task::ItemsController < ApplicationController
-  before_action :set_patients_task_item, only: %i[ show edit update destroy ]
+  before_action :set_patients_task_item, only: %i[show edit update destroy complete]
 
   # GET /patients/task/items or /patients/task/items.json
   def index
@@ -7,17 +7,19 @@ class Patients::Task::ItemsController < ApplicationController
   end
 
   # GET /patients/task/items/1 or /patients/task/items/1.json
-  def show
-  end
+  def show; end
 
   # GET /patients/task/items/new
   def new
     @patients_task_item = Patients::Task::Item.new
   end
 
-  # GET /patients/task/items/1/edit
-  def edit
+  def complete
+    @patients_task_item.update(completed_date: Time.now)
   end
+
+  # GET /patients/task/items/1/edit
+  def edit; end
 
   # POST /patients/task/items or /patients/task/items.json
   def create
@@ -25,7 +27,7 @@ class Patients::Task::ItemsController < ApplicationController
 
     respond_to do |format|
       if @patients_task_item.save
-        format.html { redirect_to @patients_task_item, notice: "Item was successfully created." }
+        format.html { redirect_to @patients_task_item, notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @patients_task_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +40,7 @@ class Patients::Task::ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @patients_task_item.update(patients_task_item_params)
-        format.html { redirect_to @patients_task_item, notice: "Item was successfully updated." }
+        format.html { redirect_to @patients_task_item, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @patients_task_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,19 +53,20 @@ class Patients::Task::ItemsController < ApplicationController
   def destroy
     @patients_task_item.destroy
     respond_to do |format|
-      format.html { redirect_to patients_task_items_url, notice: "Item was successfully destroyed." }
+      format.html { redirect_to patients_task_items_url, notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_patients_task_item
-      @patients_task_item = Patients::Task::Item.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def patients_task_item_params
-      params.fetch(:patients_task_item, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_patients_task_item
+    @patients_task_item = Patients::Task::Item.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def patients_task_item_params
+    params.fetch(:patients_task_item, {})
+  end
 end
--- app/models/patients/task/item.rb
+++ app/models/patients/task/item.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 class Patients::Task::Item < ApplicationRecord
+  belongs_to :list
+  belongs_to :item, class_name: '::Task::Item'
+
+  delegate :title, to: :item
+  delegate :body, to: :item
+
+  after_create :set_due_date
+
+  def completed?
+    completed_date.present?
+  end
+
+  def overdue?
+    due_date < Time.now
+  end
+
+  private
+
+  def set_due_date
+    update(due_date: list.start_date + item.due.days)
+  end
 end
